daf205a72ba564c903e09315c2fda747014ef7b1
Aknexad <aknejad@outlook.com>
2022-10-24 22:13:40 +0330

    make code clener

--- routers/index.js
+++ routers/index.js
@@ -2,13 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
 router.get('/', async (req, res) => {
-  const user = await prisma.user.findMany();
-
-  res.json(user);
+  res.send('welcom');
 });
 
 module.exports = router;
--- routers/login.js
+++ routers/login.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const passport = require('passport');
 
-const data = require('../data');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {});
