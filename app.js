90b68b4c198bc5ab9f030116a4f533de7357f6ea
Aknexad <aknejad@outlook.com>
2022-10-24 18:16:39 +0330

    add passport confige

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 //
 const indexRoute = require('./routers/index');
 const registerRouter = require('./routers/register');
@@ -8,16 +10,56 @@ const logoutRouter = require('./routers/logout');
 
 const app = express();
 
-// db
-const data = [];
+const data = require('./data');
+
+// passport //
+async function authenticateUser(username, password, done) {
+  const user = data.find(user => user.username === username);
+  if (user == null) {
+    return done(null, false, { message: 'user dont exist' });
+  }
+  try {
+    if (await bcrypt.compare(password, user.password)) {
+      done(null, user);
+    } else {
+      done(null, false, { message: 'passwords dont mach' });
+    }
+  } catch (error) {
+    done(error);
+  }
+}
+// passport config
+passport.use(new LocalStrategy(authenticateUser));
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+  done(
+    null,
+    users.find(user => user.id === id)
+  );
+});
 
 // middleware
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  session({
+    secret: '1234',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 10,
+    },
+  })
+);
+
+// set view engin
+// app.set('view engine', 'pug');
 
 // route
 app.use('/', indexRoute);
 app.use('/register', registerRouter);
-app.use('./login', loginRouter);
+app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 
 const port = process.env.PORT || 8080;
