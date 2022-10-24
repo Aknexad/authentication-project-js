a96733f8e4c6561dc3c2c158598b8893a5b62a06
Aknexad <aknejad@outlook.com>
2022-10-24 20:04:50 +0330

    prisma  int and add schema

---
+++ prisma/migrations/20221024162057_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `main` VARCHAR(191) NOT NULL,
+    `aothorId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_aothorId_fkey` FOREIGN KEY (`aothorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
