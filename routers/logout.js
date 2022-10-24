71059d5d178ba16c2399ec1e5082ba204c798a03
Aknexad <aknejad@outlook.com>
2022-10-24 19:27:40 +0330

    add get route logout

--- routers/logout.js
+++ routers/logout.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('logout');
+router.get('/', (req, res, next) => {
+  req.logOut(err => {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send('logout');
+  });
 });
 
 module.exports = router;
