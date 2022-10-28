4192023ebbdf2975501c7ad789acf69a95a8cb82
Aknexad <aknejad@outlook.com>
2022-10-28 16:04:53 +0330

    add .env config file

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 
+require('dotenv').config();
+
 const indexRoute = require('./routers/index');
 const registerRouter = require('./routers/register');
 const loginRouter = require('./routers/login');
