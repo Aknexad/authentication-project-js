19347ff8c5c6fc88612f547d3003205b74acb096
Aknexad <aknejad@outlook.com>
2022-10-29 17:30:52 +0330

    merg file

--- middleware/passport.strategy.js
+++ middleware/passport.strategy.js
@@ -1,11 +1,14 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+// local strategy
 async function authenticateUser(username, password, done) {
   const user = await prisma.user.findFirst({
     where: {
@@ -25,9 +28,29 @@ async function authenticateUser(username, password, done) {
     done(error);
   }
 }
+
+// JWT strategy
+const option = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+async function jwtValadation(jwt_payload, done) {
+  const user = await prisma.user.findUnique({ where: { id: jwt_payload.sub } });
+  try {
+    if (user == null) {
+      return done(null, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  } catch (error) {
+    done(error);
+  }
+}
+
 // passport config
 passport.use(new LocalStrategy(authenticateUser));
-// passport.serializeUser((user, done) => done(null, user.id));
-// passport.deserializeUser((userId, done) => {
-//   done(null, prisma.user.findUnique({ where: { id: userId } }));
-// });
+passport.use(new JwtStrategy(option, jwtValadation));
