b8ccbabc43e7cf17b9a3cb825e3b4f3b2a970160
Aknexad <aknejad@outlook.com>
2022-10-29 18:03:26 +0330

    clean the code

--- routers/register.js
+++ routers/register.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const data = require('../data');
 
 const { PrismaClient } = require('@prisma/client');
-
 const prisma = new PrismaClient();
 
 const router = express.Router();
@@ -28,32 +26,5 @@ router.post('/', async (req, res) => {
     res.status(401).send('try agen');
   }
 });
-
-// router.post('/', async (req, res) => {
-//   try {
-//     const hashedPass = await bcrypt.hash(req.body.password, 7);
-//     const user = {
-//       id: Date.now().toString(),
-//       username: req.body.username,
-//       password: hashedPass,
-//     };
-//     data.push(user);
-//     res.status(200).send('account cratted');
-//   } catch {
-//     res.status(401).send('try agen');
-//   }
-// });
-
-// router.post('/', (req, res) => {
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   const user = {
-//     id: Date.now().toString(),
-//     username: username,
-//     password: password,
-//   };
-//   data.push(user);
-//   res.status(200).json(user);
-// });
 
 module.exports = router;
